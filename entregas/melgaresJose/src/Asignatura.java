c633f20d2fd67dd106acc65dd20a8cc6bf2c58c1
Roberto Jos� Melgares Zelaya <roberto.melgares@unealum.local>
2026-03-03 13:20:40 +0100

    feat: agrega clase Asignatura y Horario

---
+++ entregas/melgaresJose/src/Asignatura.java
@@ -0,0 +1,5 @@
+class Asignatura {
+	public Asignatura(String nombre, String codigo, int creditos);
+	
+	
+}
---
+++ entregas/melgaresJose/src/Horario.java
@@ -0,0 +1,17 @@
+class Horario {
+	public Horario()
+	
+	public void agregar(Asignatura asignatura, 
+						Aula aula, 
+						Profesor profesor, 
+						DiaHora diaHoraInicio, 
+						DiaHora diaHoraFinal)
+	
+	public void mostrar()
+	public void mostrar(int dia)
+	public void mostrar(Profesor profesor)
+	public void mostrar(Aula aula)
+	public void mostrar(Asignatura asignatura)
+	
+	public void 
+}	
