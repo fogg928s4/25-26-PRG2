deced524fbe400333590369b8a1061aa6c908655
Jose Melgares <rjmzelaya@gmail.com>
2026-03-17 13:20:04 +0100

    feat: constructores iniciales

--- entregas/melgaresJose/src/reto-004/Fraccion.java
+++ entregas/melgaresJose/src/reto-004/Fraccion.java
@@ -1,4 +1,14 @@
 class Fraccion {
     private double numerador;
     private double denominador;
+
+    public Fraccion(double numerador, double denominador) {
+        this.denominador = denominador;
+        this.numerador = numerador;
+    }
+
+    public Fraccion(double numeroEntero) {
+        numerador = numeroEntero;
+        denominador = 1;
+    }
 }
