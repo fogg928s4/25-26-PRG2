45c0888cd4163d5c29baaa896206500c20fc7e2d
Jose Melgares <rjmzelaya@gmail.com>
2026-05-12 00:12:41 +0200

    feat: adds method for restarting game

--- entregas/melgaresJose/src/Blackjack.java
+++ entregas/melgaresJose/src/Blackjack.java
@@ -41,8 +41,12 @@ class Blackjack {
             mano.pedirCarta(baraja);
         }
     }
-    private void reiniciar() {
 
+    private void reiniciar() {
+        puntaje = 0;
+        baraja = new Baraja();
+        mano = new Mano();
+        repartir();
     }
 
     private boolean seHaPasado() {
